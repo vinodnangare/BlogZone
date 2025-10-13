8ffc383fd3bdef2798c9c4763bba7311ebbe4f66
Vinod Nangare <vinodnangare8@gmail.com>
2025-10-13 21:52:01 +0530

    feat: Add API method

--- server/index.js
+++ server/index.js
@@ -1,13 +1,17 @@
-import express from 'express';
+import express, { json } from 'express';
 import dotenv from 'dotenv';
-import mongoose from 'mongoose';
-
+import mongoose, { get } from 'mongoose';
+import cors from 'cors';
+import { postRegister ,getUsers, getBlogs ,postBlogs, getlogin, getBlog, userBlogs,deleteBlog} from './controllers/controller.js';
 dotenv.config({path:'../.env'});
+
+
 
 const app= express();
 app.use(express.json());
-
-const conn=async (req,res)=>{
+app.use(json());
+app.use(cors());
+const conn=async ()=>{
     try{
     await mongoose.connect(process.env.DB_URL);
    
@@ -29,6 +33,14 @@ app.get('/',(req,res)=>{
     res.send("Tiny Blog Server Has Started")
 });
 
+app.post('/register', postRegister);
+app.post('/login',getlogin);
+app.get('/users',getUsers);
+app.post('/blogs',postBlogs);
+app.get('/blogs/:id',getBlog)
+app.get('/blogs/user/:id',userBlogs);
+app.get('/blogs',getBlogs);
+app.delete('/blogs/:id',deleteBlog);
 const PORT=process.env.PORT;
 console.log(PORT)
 app.listen(PORT ,()=>{

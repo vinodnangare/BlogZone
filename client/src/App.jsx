08e5150733a8b9b7a168ff3acafa9b6742e9ba8a
Vinod Nangare <vinodnangare8@gmail.com>
2025-10-14 23:44:05 +0530

    feat:Add Home Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import axios from 'axios'
+import './index.css'
+import { useState,useEffect } from 'react'
+import Navbar from './components/Navbar.jsx'
+import { useNavigate } from 'react-router'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+const[publishedBlogs,setPublishedBlogs]=useState([]);
 
+const fetchPublishedBlogs=async()=>{
+  try{
+    const response=await axios.get(`${import.meta.env.VITE_DB_URL}/published`);
+    console.log(response.data);
+    if(response.data.sucess){
+      setPublishedBlogs(response.data.data);
+      
+    }
+    else{
+      alert("Failed to fetch published blogs")
+    }
+
+  }
+catch(e){
+    console.log("Failed to fetch published blogs")
+}
+
+
+}
+
+const navigate=useNavigate();
+
+ useEffect(() => {
+    fetchPublishedBlogs();
+  }, []); 
   return (
     <>
+    <Navbar />
+
+
+{publishedBlogs.map((blog)=>(
+  <div>
+  <div key={blog._id} className='border-2 border-black p-5 m-5'>
+    <h2 className='text-2xl font-bold'>{blog.title}</h2>
+    <p className='text-gray-600'>By: {blog.authorId.username} ({blog.authorId.email})</p>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <button className='mt-3 p-2 bg-amber-300 rounded-[10px] cursor-pointer' onClick={()=>navigate(`blog/${blog._id}`)}  >Read More</button>
+  </div>
+
+  </div>
+
+  </div>
+ 
+))}
     </>
   )
 }

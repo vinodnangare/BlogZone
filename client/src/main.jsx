232aec9050382e6d15b60cc8f55970eab53498cc
Vinod Nangare <vinodnangare8@gmail.com>
2025-10-17 08:56:13 +0530

    fix: proper react-router setup

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -6,23 +6,17 @@ import Login from './pages/Login.jsx'
 import Register from './pages/Register.jsx'
 import Blogs from './pages/Blogs.jsx'
 import DetailedBlog from './pages/DetailedBlog.jsx'
-
-import { Router, Routes, Route } from 'react-router'
-import { createBrowserHistory } from 'history'
-
-const history = createBrowserHistory()
+import { BrowserRouter, Route, Routes } from 'react-router'
 
 createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Router navigator={history} location={history.location}>
-      <Routes>
-        <Route path="/blog/:id" element={<DetailedBlog />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/blogs" element={<Blogs />} />
-        <Route path="/" element={<App />} />
-        <Route path="*" element={<div>404 Not Found</div>} />
-      </Routes>
-    </Router>
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path='/blog/:id' element={<DetailedBlog />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/register' element={<Register />} />
+      <Route path='/blogs' element={<Blogs />} />
+      <Route path='/' element={<App />} />
+      <Route path='*' element={<div>404 Not Found</div>} />
+    </Routes>
+  </BrowserRouter>
 )

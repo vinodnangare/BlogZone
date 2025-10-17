10453ae90d4d37e9a37b9c5d2d9da8738816be5b
Vinod Nangare <vinodnangare8@gmail.com>
2025-10-17 06:50:23 +0530

    feat: Home page updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -41,9 +41,18 @@ const navigate=useNavigate();
 
 {publishedBlogs.map((blog)=>(
   <div>
+  
   <div key={blog._id} className='border-2 border-black p-5 m-5'>
-    <h2 className='text-2xl font-bold'>{blog.title}</h2>
+      <div className='flex justify-between items-center mb-4'>
+       <h2 className='text-2xl font-bold static '>{blog.title}</h2>
+         <p className='text-gray-800 border-2 border-amber-200 w-fit px-2 py-0 rounded-2xl bg-green-400 text-[10px]'>{blog.state}</p>
+    </div>
+
+  
     <p className='text-gray-600'>By: {blog.authorId.username} ({blog.authorId.email})</p>
+    <div className='flex flex-row-reverse relative mt-0 '>
+
+    </div>
       <div>
     <button className='mt-3 p-2 bg-amber-300 rounded-[10px] cursor-pointer' onClick={()=>navigate(`blog/${blog._id}`)}  >Read More</button>
   </div>

d5f22f8ed377a6335dd96f7e1bb76f39e3f7e47b
Vinod Nangare <vinodnangare8@gmail.com>
2025-10-17 08:28:53 +0530

    Routes updated

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,28 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Login from './pages/Login.jsx'
+import Register from './pages/Register.jsx'
+import Blogs from './pages/Blogs.jsx'
+import DetailedBlog from './pages/DetailedBlog.jsx'
+
+import { Router, Routes, Route } from 'react-router'
+import { createBrowserHistory } from 'history'
+
+const history = createBrowserHistory()
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <React.StrictMode>
+    <Router navigator={history} location={history.location}>
+      <Routes>
+        <Route path="/blog/:id" element={<DetailedBlog />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/" element={<App />} />
+        <Route path="*" element={<div>404 Not Found</div>} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
 )

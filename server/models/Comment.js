5125a2c6b680d1c0571107d6f9adcb463e399249
Vinod Nangare <vinodnangare8@gmail.com>
2025-11-13 21:10:36 +0530

    feat: Add Schema for Commit

--- server/models/Comment.js
+++ server/models/Comment.js
@@ -1,15 +1,13 @@
 import mongoose from "mongoose";
-import { Schema,Model } from "mongoose";
+import { Schema } from "mongoose";
 
-const commentSchema=new Schema({
+const commentSchema = new Schema({
+    content: {type: String, required: true},
+    blogId: {type: Schema.Types.ObjectId, ref: "Blog", required: true},
+    userId: {type: Schema.Types.ObjectId, ref: "User", required: true},
+    createdAt: {type: Date, default: Date.now}
+});
 
-    content:{type:String ,required:true},
-    blogId:{type:Schema.Types.ObjectId ,ref:"Blog",required:true},
-    userId:{type:Schema.Types.ObjectId ,ref:"User",required:true},
-    createdAt:{type:Date ,default:Date.now}
+const Comment = mongoose.model("Comment", commentSchema);
 
-})
-
-const comment =mongoose.model("Comment",commentSchema);
-
-export {comment};
+export { Comment };

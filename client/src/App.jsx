9ce77927996e0abd210491780aa8203d84606d12
Vinod Nangare <vinodnangare8@gmail.com>
2025-11-13 21:24:15 +0530

    fix:Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,62 +1,53 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
-import './index.css'
-import Navbar from './components/Navbar.jsx'
-import { useNavigate } from 'react-router'
+import React, { useEffect, useState } from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import axios from "axios";
+import "./index.css";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Blogs from "./pages/Blogs";
+import CreateBlog from "./pages/CreateBlog";
+import EditBlog from "./pages/EditBlog";
+import DetailedBlog from "./pages/DetailedBlog";
+import Navbar from "./components/Navbar";
 
 function App() {
-  const [publishedBlogs, setPublishedBlogs] = useState([])
-  const navigate = useNavigate()
-
+  const [publishedBlogs, setPublishedBlogs] = useState([]);
+  const navigate = useNavigate();
+  console.log(`${import.meta.env.VITE_DB_URL}/published`);
   const fetchPublishedBlogs = async () => {
     try {
-      const response = await axios.get(`${import.meta.env.VITE_DB_URL}/published`)
-      console.log(response.data)
+      const response = await axios.get(
+        `${import.meta.env.VITE_DB_URL}/published`
+      );
+      console.log(response.data);
       if (response.data.sucess) {
-        setPublishedBlogs(response.data.data)
+        setPublishedBlogs(response.data.data);
       } else {
-        alert('Failed to fetch published blogs')
+        alert("Failed to fetch published blogs");
       }
     } catch (e) {
-      console.log('Failed to fetch published blogs')
+      console.log("Failed to fetch published blogs");
     }
-  }
+  };
 
   useEffect(() => {
-    fetchPublishedBlogs()
-  }, [])
+    fetchPublishedBlogs();
+  }, []);
 
   return (
     <>
       <Navbar />
-
-      {publishedBlogs.map((blog) => (
-        <div key={blog._id} className="border-2 border-black p-5 m-5">
-          <div className="flex justify-between items-center mb-4">
-            <h2 className="text-2xl font-bold static ">{blog.title}</h2>
-            <p className="text-gray-800 border-2 border-amber-200 w-fit px-2 py-0 rounded-2xl bg-green-400 text-[10px]">
-              {blog.state}
-            </p>
-          </div>
-
-          <p className="text-gray-600">
-            By: {blog.authorId.username} ({blog.authorId.email})
-          </p>
-
-          <div className="flex flex-row-reverse relative mt-0 "></div>
-
-          <div>
-            <button
-              className="mt-3 p-2 bg-amber-300 rounded-[10px] cursor-pointer"
-              onClick={() => navigate(`blog/${blog._id}`)}
-            >
-              Read More
-            </button>
-          </div>
-        </div>
-      ))}
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/create" element={<CreateBlog />} />
+        <Route path="/edit/:id" element={<EditBlog />} />
+        <Route path="/blog/:slug" element={<DetailedBlog />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

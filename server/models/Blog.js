f0c6c453b2c4caea95e111e5c358ccc9d7074249
Vinod Nangare <vinodnangare8@gmail.com>
2025-11-13 21:09:40 +0530

    feat:Add blog type Schema

--- server/models/Blog.js
+++ server/models/Blog.js
@@ -1,13 +1,18 @@
-import {Model, Schema} from "mongoose";
-import mongoose from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
-const blogSchema=new Schema({
-    title:{type:String ,required:true},
-    content:{type:String ,required:true},
-    authorId:{type:Schema.Types.ObjectId ,ref:"User",required:true},
-    state:{type:String ,enum:["draft","published"],default:"draft"},
-    createdAt:{type:Date ,default:Date.now}
-})
-const Blog=mongoose.model("Blog",blogSchema);
+const blogSchema = new Schema({
+    title: { type: String, required: true },
+    slug: { type: String, unique: true },
+    content: { type: String, required: true },
+    authorId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    type: { 
+      type: String, 
+      enum: ["Technology", "Health", "Education", "Travel", "Animal", "Lifestyle", "Business", "Other"],
+      default: "Technology" 
+    },
+    state: { type: String, enum: ["draft", "published"], default: "draft" },
+    createdAt: { type: Date, default: Date.now }
+});
 
-export {Blog};
+const Blog = mongoose.model("Blog", blogSchema);
+export { Blog };

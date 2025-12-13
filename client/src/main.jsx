8596356fcba930501b4f96f2bf7c275b8ad1d8f3
Vinod Nangare <vinodnangare8@gmail.com>
2025-12-13 23:59:46 +0530

    fix:Not Found

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import axios from 'axios';
 import App from './App';
 import './index.css';
@@ -21,8 +21,8 @@ if (token && token !== 'undefined' && token !== 'null') {
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
